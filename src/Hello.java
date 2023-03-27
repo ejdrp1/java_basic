854e1d1a638aaf37b6a34cdd892d0243f1d671bf
ejdrp1 <ejdrp1@naver.com>
2023-03-27 10:38:35 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요!");
+
+        // Ctrl + Alt + V
+        int i = 100;
+        String 김철수 = "김철수";
+        double rn = Math.random();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
